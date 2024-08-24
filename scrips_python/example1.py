165caf250cc0702d37467f0d2b0e19f8e6b21983
Jonathan <jonathanquenoran4@gmail.com>
2024-08-23 21:02:31 -0500

    Scrip python 1

---
+++ scrips_python/example1.py
@@ -0,0 +1,16 @@
+#Este es un comentario de linea
+'''
+Esto e s un cometario de bloque
+'''
+
+data = '''
+Eto es una variable string de un bloque
+'''
+
+n1 = 10
+n2 = 20
+suma = n1 + n2
+print(suma)
+print("La suma es:", suma)
+print(f"La suma es: {suma}")
+print("La smma es: ",n1 + n2)
